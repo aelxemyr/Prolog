3e07e7cfaddf97c971592307a7865e5191b8e0b9
teron14 <vulpinoctum@gmail.com>
2016-02-01 10:28:40 -0700

    Revert "Prolog stuff I already did"
    This reverts commit 8a721f6a6b9d2c7ff4d33956ede7fdabd748b629.

--- ch3ex1.pl
+++
@@ -1,7 +0,0 @@
-in(X,Y):- contains(X,Y).
-in(X,Z):- contains(X,Y),
-	contains(Y,Z).
-
-contains(katarina,olga).
-contains(olga,natasha).
-contains(natasha,irina).
--- descend2.pl
+++
@@ -1,8 +0,0 @@
-child(martha,charlotte).
-child(charlotte,caroline).
-child(caroline,laura).
-child(laura,rose).
-
-descend(X,Y) :- descend(Z,Y),child(X,Z).
-
-descend(X,Y) :- child(X,Y).
